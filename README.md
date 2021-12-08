d7adc10bfb040a6c93d1a385559ac54d91f2db46
faizal <faizal@beon.co.id>
2021-12-08 13:48:30 +0700

    init

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # jagoanhosting-backup-cpanel-hosting-s3
 You can use this script to backup your cpanel hosting (Full Backup) to Object Storage
+
+#### Basic Flow:
+1. Doing Full Backup on Cpanel using UAPI
+2. Waiting for Full Backup Done
+3. Upload to S3
+
+#### Main Config
+```shell script
+S3KEY="******"
+S3SECRET="*****"
+bucket="****"
+```
+
+Get Your Credential S3 and create bucket to store your backup
+
+if your backup not locate on user folder, you can change
+```shell script
+path="./"
+```
+
+#### Installation
+1. Upload **s3-backup.sh** to /home/{user}
+2. Setup Config
+3. Create Cron Job, your can set as you like. (daily, monthly, ETC). Every cron execute backup will process backup to S3
+```shell script
+
+```
+
+#### How to get S3 Storage
+You can get S3 Storage here: 
+
+[S3 Object Storage](https://www.jagoanhosting.com/cloud-object-storage/?utm_source=github&utm_medium=readme&utm_campaign=BackupS3&utm_id=BackupS3)
+with only 1 USD with unlimited data transfer
---
+++ s3-backup.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+username=$(whoami)
+path="./"
+
+## Insert S3 Credentials
+S3KEY="******"
+S3SECRET="*****"
+bucket="****"
+
+## S3 Config Path & Bucket
+dream_path="/${username}/$(date +%F)/"
+
+function BackupToS3 {
+
+  path="$1"
+  file="$2"
+  S3_path="$3"
+  bucket="$4"
+
+  full_path="${bucket}${S3_path}${file}"
+  date=`date -R -u`
+
+  content_type='application/x-compressed-tar'
+  string="PUT\n\n${content_type}\n${date}\n/${full_path}"
+  signature=$(echo -en "${string}" | openssl sha1 -hmac "${S3SECRET}" -binary | base64)
+
+  curl -X PUT -T "${path}/${file}" \
+    -H "Host: ${bucket}.global.jagoanstorage.com" \
+    -H "Date: ${date}" \
+    -H "Content-Type: ${content_type}" \
+    -H "Authorization: AWS ${S3KEY}:${signature}" \
+    "http://${bucket}.global.jagoanstorage.com${S3_path}${file}"
+}
+
+# Create Backup using Cpanel UAPI
+uapi Backup fullbackup_to_homedir
+
+while [ -z "$file" ]
+do
+  sleep 2 # or less like 0.2
+  file=$(ls | grep ${username}.tar.gz | tail -n 1)
+  file_temp=$( basename "${HOME}/${file}" .tar.gz)
+  check=$(ls | grep -E "${file_temp}$")
+  if [ ! -z "$check" ]
+  then
+    file=""
+    echo "Waiting For Backup"
+  fi
+done
+
+BackupToS3 "${path}" "${file}" "${dream_path}" "${bucket}"
+cd $HOME
+rm -f ${file}
